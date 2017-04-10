375c6d0d231513a6e150b78d48c894d3945f213a
js_suse <373116645@qq.com>
2017-04-10 12:12:44 +0800

    发送附件

--- src/main/java/com/building/SendDocToMyKindle.java
+++ src/main/java/com/building/SendDocToMyKindle.java
@@ -1,5 +1,8 @@
 package com.building;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -45,19 +48,29 @@ public class SendDocToMyKindle {
 
 		try {
 
+			// [01]
 			SendMailProvider mailProvider = sendDocToMyKindle.getMailProvider();
 
-			long time = System.currentTimeMillis();
+			String fileName = "Notes.txt";
+			String filePath = "/root/Documents/" + fileName;
 
+			// [02]
 			MailEntity mail = new MailEntity();
 			mail.setTo("qianshijinsheng@gmail.com");
-			mail.setSubject("Test Java Mail");
-			mail.setText("Timestamp : " + String.valueOf(time));
+			mail.setSubject(fileName);
+			mail.setText("Timestamp : " + String.valueOf(System.currentTimeMillis()));
 			// 为from字段赋值
 			mail.setFrom(mailProvider.getJavaMailSender().getUsername());
 
-			mailProvider.sendSimpleMail(mail);
+			Map<String, String> attachment = new HashMap<String, String>();
+			attachment.put(fileName, filePath);
+			mail.setAttachment(attachment);
 
+			// [03]
+			mailProvider.sendMimeMail(mail);
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		} finally {
 			if (null != sendDocToMyKindle.getApplicationContext()) {
 				sendDocToMyKindle.getApplicationContext().close();
